afeb45b20630350c9d52cd11ce7ab37fc3895fd5
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-08-12 10:11:58 +0200

    feat: Ajout du cast IntBool

---
+++ src/Casts/AsIntBool.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework - Eloquent ORM Adapter.
+ *
+ * (c) 2023 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BlitzPHP\Wolke\Casts;
+
+use BlitzPHP\Wolke\Contracts\Castable;
+use BlitzPHP\Wolke\Contracts\CastsAttributes;
+use BlitzPHP\Wolke\Model;
+
+class AsIntBool implements Castable
+{
+    /**
+     * Get the caster class to use when casting from / to this cast target.
+     */
+    public static function castUsing(array $arguments): CastsAttributes
+    {
+        return new class () implements CastsAttributes {
+            public function get(Model $model, string $key, mixed $value, array $attributes): mixed
+            {
+				return (bool) $attributes[$key];
+            }
+
+            public function set(Model $model, string $key, mixed $value, array $attributes): mixed
+            {
+                return [$key => (int) $value];
+            }
+        };
+    }
+}
